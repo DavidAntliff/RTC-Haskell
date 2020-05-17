95ca308b5e2d268798a12a66668c8a9bf564238b
David Antliff <david@pitchblende.co.nz>
2020-05-17 12:41:55 +1200

    Tidy up Matrix abstraction.

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -11,15 +11,14 @@ import Linear ( M22, M33, M44
               , V2 (V2), V3 (V3), V4 (V4)
               , R2, R3, R4
               , _x, _y, _z, _w)
-import Linear.Matrix (identity)
 
 type Row2 = (Double, Double)
 type Row3 = (Double, Double, Double)
 type Row4 = (Double, Double, Double, Double)
 
-data Matrix22 = Matrix22 (M22 Double) deriving (Show)
-data Matrix33 = Matrix33 (M33 Double) deriving (Show)
-data Matrix44 = Matrix44 (M44 Double) deriving (Show)
+newtype Matrix22 = Matrix22 (M22 Double) deriving Show
+newtype Matrix33 = Matrix33 (M33 Double) deriving Show
+newtype Matrix44 = Matrix44 (M44 Double) deriving Show
 
 matrix22 :: (Row2, Row2) -> Matrix22
 matrix22 ( (a00, a01)
@@ -36,53 +35,27 @@ matrix44 ( (a00, a01, a02, a03)
          , (a20, a21, a22, a23)
          , (a30, a31, a32, a33) ) = Matrix44 (V4 (V4 a00 a01 a02 a03) (V4 a10 a11 a12 a13) (V4 a20 a21 a22 a23) (V4 a30 a31 a32 a33) :: M44 Double)
 
---elementAt :: (Int, Int) -> Matrix44 -> Double
---elementAt (row, col) m = getRowElement col $ getRow row m
-
 class MatrixAccess a where
   elementAt :: (Int, Int) -> a -> Double
-  
+
 instance MatrixAccess Matrix22 where
-  --elementAt (row, col) m = getRowElement col $ getRow row m
   elementAt (row, col) (Matrix22 m) = getVectorElement2 col $ getVectorElement2 row m
 
 instance MatrixAccess Matrix33 where
   elementAt (row, col) (Matrix33 m) = getVectorElement3 col $ getVectorElement3 row m
 
 instance MatrixAccess Matrix44 where
-  --elementAt (row, col) m = getRowElement col $ getRow row m
   elementAt (row, col) (Matrix44 m) = getVectorElement4 col $ getVectorElement4 row m
-  
 
 -- Internal
+getVectorElement2 :: Int -> V2 a -> a 
+getVectorElement2 idx v = v ^. ind2 idx
 
-getVectorElement2 idx v@(V2 _ _) = v ^. ind2 idx
-getVectorElement3 idx v@(V3 _ _ _) = v ^. ind3 idx
-getVectorElement4 idx v@(V4 _ _ _ _) = v ^. ind4 idx
+getVectorElement3 :: Int -> V3 a -> a 
+getVectorElement3 idx v = v ^. ind3 idx
 
-
---getVectorElement :: Int -> (t a) -> a
---getVectorElement idx v&(V4 Double _ _ _ _) = v ^. (ind4 idx)
---getVectorElement idx v&(V2 Double _ _) = v ^. (ind2 idx)
---getVectorElement idx (V4 v) = v ^. ind idx
-
---class VectorAccess a where
---  getVectorElement :: Int -> a -> b
---
---instance VectorAccess Matrix22 where
---  getVectorElement idx v = v ^. (ind2 idx)
---  
---instance VectorAccess (V2 Double) where
---  getVectorElement idx v = v ^. (ind2 idx)
---
---instance VectorAccess (V4 Double) where
---  getVectorElement idx v = v ^. (ind4 idx)
-
---getRow :: Int -> Matrix44 -> V4 Double
---getRow row (Matrix44 m) = m ^. (ind row)
---
---getRowElement :: Int -> V4 Double -> Double
---getRowElement col v = v ^. (ind col)
+getVectorElement4 :: Int -> V4 a -> a 
+getVectorElement4 idx v = v ^. ind4 idx
 
 -- map V2 element indices to Lens
 ind2 :: (Eq a1, Num a1, Functor f, R2 t) => a1 -> (a2 -> f a2) -> t a2 -> f (t a2)
