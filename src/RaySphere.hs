962bce88778cf279b3c2d0ad0e223b2c86c8c3cf
David Antliff <david@pitchblende.co.nz>
2020-05-31 18:47:38 +1200

    Implement ray intersection with unit sphere at origin.

--- src/RaySphere.hs
+++ src/RaySphere.hs
@@ -4,7 +4,11 @@ module RaySphere ( Ray
                  , ray
                  , position
                  , sphere
+                 , Intersection(..)
                  , intersect
+                 , IntersectionList
+                 , intersections
+                 , intersectionListCount
                  ) where
 
 import Control.Exception
@@ -15,8 +19,6 @@ data Ray = Ray { origin :: Quadruple     -- point
                , direction :: Quadruple  -- vector
                } deriving (Show)
 
-data Sphere = Sphere deriving (Show)
-
 ray :: Quadruple -> Quadruple -> Ray
 ray o d = assert (isPoint o)
           $ assert (isVector d)
@@ -25,19 +27,34 @@ ray o d = assert (isPoint o)
 position :: Ray -> Double -> Quadruple
 position (Ray o d) t = o |+| d |* t
 
-sphere :: Sphere
+data Shape = Sphere | Obloid deriving (Eq, Show)
+
+sphere :: Shape
 sphere = Sphere
+
+data Intersection = Intersection { intersectionT :: Double
+                                 , intersectionObject :: Shape
+                                 } deriving (Eq, Show)
+
+type IntersectionList = [Intersection]
 
 -- # the vector from the sphere's center, to the ray origin
 -- remember: the sphere is centered at the world origin
-intersect :: Sphere -> Ray -> [Double]
-intersect s r
+intersect :: Shape -> Ray -> IntersectionList
+intersect s@Sphere r
   | discriminant < 0 = []
   | otherwise = let t1 = (-b - sqrt discriminant) / (2 * a)
                     t2 = (-b + sqrt discriminant) / (2 * a)
-                in [t1, t2]
+                in intersections [ Intersection t1 s, Intersection t2 s ]
   where sphere_to_ray = origin r |-| point 0 0 0
         a = dot (direction r) (direction r)
         b = 2 * dot (direction r) sphere_to_ray
         c = dot sphere_to_ray sphere_to_ray - 1
         discriminant = b * b - 4 * a * c
+intersect _ r = []
+
+intersections :: [Intersection] -> IntersectionList
+intersections = id
+
+intersectionListCount :: IntersectionList -> Int
+intersectionListCount = length
--- test/RaySphere/Tests.hs
+++ test/RaySphere/Tests.hs
@@ -3,6 +3,8 @@ module RaySphere.Tests (unitTests) where
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Control.Lens
+
 import RaySphere
 import Quadruple
 import Math (almostEqual)
@@ -28,22 +30,45 @@ unitTests = testGroup "Ray-Sphere Unit Tests"
       let r = ray (point 0 0 (-5)) (vector 0 0 1)
           s = sphere
           xs = intersect s r
-      in [ testCase "count" $ assertEqual [] (length xs) 2
-         , testCase "first" $ assertEqual [] (head xs) 4.0 
-         , testCase "second" $ assertEqual [] (head . tail $ xs) 6.0
+      in [ testCase "count" $ assertEqual [] 2 (intersectionListCount xs)
+         , testCase "first" $ assertEqual [] 4.0 (intersectionT $ xs ^?! element 0)
+         , testCase "second" $ assertEqual [] 6.0 (intersectionT $ xs ^?! element 1)
          ] 
   , testGroup "A ray intersects a sphere at a tangent" $
       let r = ray (point 0 1 (-5)) (vector 0 0 1)
           s = sphere
           xs = intersect s r
-      in [ testCase "count" $ assertEqual [] (length xs) 2
-         , testCase "first" $ assertEqual [] (head xs) 5.0 
-         , testCase "second" $ assertEqual [] (head . tail $ xs) 5.0
+      in [ testCase "count" $ assertEqual [] 2 (intersectionListCount xs)
+         , testCase "first" $ assertEqual [] 5.0 (intersectionT $ xs ^?! element 0) 
+         , testCase "second" $ assertEqual [] 5.0 (intersectionT $ xs ^?! element 1)
          ] 
   , testCase "A ray misses a sphere" $
       let r = ray (point 0 2 (-5)) (vector 0 0 1)
           s = sphere
           xs = intersect s r
       in assertEqual [] (length xs) 0
+  , testGroup "An intersection encapsulates t and object" $
+      let s = sphere
+          i = Intersection 3.5 s
+      in [ testCase "t" $ assertEqual [] 3.5 (intersectionT i)
+         , testCase "object" $ assertEqual [] s (intersectionObject i) 
+         ]
+  , testGroup "Aggregating intersections" $
+      let s = sphere
+          i1 = Intersection 1 s
+          i2 = Intersection 2 s
+          xs = intersections [i1, i2]
+      in [ testCase "t" $ assertEqual [] 2 (intersectionListCount xs)
+         , testCase "first" $ assertEqual [] 1 (intersectionT $ xs ^?! element 0) 
+         , testCase "second" $ assertEqual [] 2 (intersectionT $ xs ^?! element 1) 
+         ]
+  , testGroup "Intersect sets the object on the intersection" $
+      let r = ray (point 0 0 (-5)) (vector 0 0 1)
+          s = sphere
+          xs = intersect s r
+      in [ testCase "t" $ assertEqual [] 2 (intersectionListCount xs)
+         , testCase "first" $ assertEqual [] s (intersectionObject $ xs ^?! element 0) 
+         , testCase "second" $ assertEqual [] s (intersectionObject $ xs ^?! element 1) 
+         ]
   ]
 
